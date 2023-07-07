433abeb047cf02a4a19b1869b7536035a8d96d63
Alex87464 <alexoliva87464@gmail.com>
2023-07-07 00:22:47 -0300

    Created index.js to export the components of the ui, auth & heroes pages

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 
+
+import { MarvelPage, DcPage } from "../heroes/"
+import { LoginPage } from '../auth';
 import { Navbar } from "../ui";
 
-import { MarvelPage } from "../heroes/pages/MarvelPage"
-import { DcPage } from '../heroes/pages/DcPage';
-import { LoginPage } from '../auth/pages/LoginPage';
 
 
 export const AppRouter = () => {
