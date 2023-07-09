e4bce43035f5b071accdaba0cd321c957dfa7b36
Alex87464 <alexoliva87464@gmail.com>
2023-07-09 01:27:00 -0300

    Implemented the user state for the app

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,15 +3,27 @@ import { AuthContext } from './AuthContext';
 import { AuthReducer } from './AuthReducer';
 import { types } from '../types/types';
 
-const initialState = {
-  logged: false,
+// const initialState = {
+//   logged: false,
+// }
+
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') );
+
+  return {
+    logged: !!user,
+    user: user,
+  }
 }
 
 export const AuthProvider = ({ children }) => {
 
-  const [authState, dispatch] = useReducer(AuthReducer, initialState);
+  const [ authState, dispatch ] = useReducer( AuthReducer, {}, init );
 
   const login = async( name = '' ) => {
+
+    const user = { id: 'ABC', name };
+
     const action = {
       type: types.login,
       payload: {
@@ -19,6 +31,8 @@ export const AuthProvider = ({ children }) => {
         name: name
       }
     }
+
+    localStorage.setItem( 'user', JSON.stringify( user ) );
 
     dispatch(action);
   }
