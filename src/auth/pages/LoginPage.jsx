bbe805615265f39e6ba3ef293dc4403fe6e922d6
Alex87464 <alexoliva87464@gmail.com>
2023-07-10 15:17:29 -0300

    Creater new UI for the login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,36 +1,43 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom"
-import { AuthContext } from "../context/AuthContext";
-
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
-
   const navigate = useNavigate();
 
-
-  const { login } = useContext( AuthContext );
+  const { login } = useContext(AuthContext);
   const handleLogin = () => {
-
     const lastPath = localStorage.getItem('lastPath') || '/';
-    
-    login( 'Alex Oliva' );
+
+    login('Persona');
 
     navigate(lastPath, {
-      replace:true
+      replace: true,
     });
-  }
+  };
 
   return (
     <div className="container mt-5">
-      <h1>Login</h1>
-      <hr/>
-
-      <button
-        className="btn btn-primary"
-        onClick={ handleLogin }
-      >
-        Login
-      </button>
+      <div className="login-box">
+        <h2>Login</h2>
+        <form>
+          <div className="user-box">
+            <input type="text" name="" required="" />
+            <label>Username</label>
+          </div>
+          <div className="user-box">
+            <input type="password" name="" required="" />
+            <label>Password</label>
+          </div>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+          </a>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
