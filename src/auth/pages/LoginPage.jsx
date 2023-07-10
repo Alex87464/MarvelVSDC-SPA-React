2da9b08f6371e19b62502d5a0871382692a2895b
Alex87464 <alexoliva87464@gmail.com>
2023-07-10 17:49:24 -0300

    Fixed the login button top redirect only if the input label git status

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,12 +1,23 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
   const navigate = useNavigate();
-
   const { login } = useContext(AuthContext);
+
+  const [username, setUsername] = useState('');
+
+  const handleUsernameChange = event => {
+    setUsername(event.target.value);
+  };
+
   const handleLogin = () => {
+    if (username.trim() === '') {
+      // El campo de entrada está vacío, no se realiza el inicio de sesión
+      return;
+    }
+
     const lastPath = localStorage.getItem('lastPath') || '/';
 
     login('Persona');
@@ -25,12 +36,19 @@ export const LoginPage = () => {
             <input
               type="text"
               name="username"
-              required={false}
+              value={username}
+              onChange={handleUsernameChange}
+              required={true}
               autoComplete="off"
             />
             <label>Username</label>
           </div>
-          <button className="login-button" href="#" onClick={handleLogin}>
+          <button
+            className="login-button"
+            href="#"
+            onClick={handleLogin}
+            disabled={username.trim() === ''}
+          >
             <span></span>
             <span></span>
             <span></span>
